3a5bf6679c6130eadbdcccea5596f80acc60770b
Renan <orenan124@gmail.com>
2023-05-17 10:21:41 -0300

    setup

---
+++ src/test/java/com/toolschallenge/pagamento/pagamento/PagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toolschallenge.pagamento.pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
